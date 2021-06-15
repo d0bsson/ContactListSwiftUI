dab82c23e6beade8846c983e93cbb9cee81b4323
David <ber.dev@yandex.ru>
2021-06-15 19:23:06 +0300

    start

---
+++ ContactListSwiftUI/DataManager.swift
@@ -0,0 +1,41 @@
+//
+//  DataManager.swift
+//  ContactListSwiftUI
+//
+//  Created by Дэвид Бердников on 15.06.2021.
+//
+
+import Foundation
+
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    var names = [
+        "John", "Aaron", "Tim", "Ted",
+        "Steven", "Sharon", "Nicola",
+        "Allan", "Bruce", "Carl"
+    ]
+    
+    let surnames = [
+        "Smith", "Dow", "Isaacson", "Pennyworth",
+        "Jankin", "Butler", "Black", "Robertson",
+        "Murphy", "Williams"
+    ]
+    
+    let emails = [
+        "jjjj@mail.ru", "aaaa@mail.ru", "eeee@mail.ru",
+        "hhhh@mail.ru", "wwww@mail.ru", "mmmm@mail.ru",
+        "xxxx@mail.ru", "pppp@mail.ru", "ssss@mail.ru",
+        "llll@mail.ru"
+    ]
+    
+    let phones = [
+        "745396026", "145036843", "287185656", "293520954",
+        "562880225", "738594072", "825654134", "645898473",
+        "432349845", "459477625"
+    ]
+    
+    private init() {}
+}
---
+++ ContactListSwiftUI/Model/Person.swift
@@ -0,0 +1,55 @@
+//
+//  Person.swift
+//  ContactListSwiftUI
+//
+//  Created by Дэвид Бердников on 15.06.2021.
+//
+
+import Foundation
+
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+
+extension Person {
+    static func getContactList() -> [Person] {
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(names.count, surnames.count, emails.count, phones.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phone: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone"
+    case email = "tray"
+}
+    
+
+
